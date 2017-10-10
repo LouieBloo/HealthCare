54504952076b42c60b66c4c24b1c32fcf4731e27
Luke Leggio <laleggio@ucdavis.edu>
2017-10-09 21:16:04 -0700

    First commit from box

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  res.render('index', { title: 'Express' });
+
+});
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+
+  res.render('index', { title: 'Post express' });
+  
+});
+
+module.exports = router;
