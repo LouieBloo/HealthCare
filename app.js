4bdb7fd8541b4b600d837ac8afa6b98754a4e83a
LouieBloo <laleggio@ucdavis.edu>
2017-10-10 23:29:41 -0700

    Basic template up, more ejs testing

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var config = require('./config');
 var database = require('./database');
 
 var app = express();
@@ -26,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-
+app.locals.configFile = config;
 
 app.use('/', index);
 app.use('/users', users);
