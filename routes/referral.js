bc81ddb5f2ac87aab6f5c460b8b85cf84ad78e53
LouieBloo <laleggio@ucdavis.edu>
2017-10-23 00:41:13 -0700

    Work on showing referrals

--- routes/referral.js
+++ routes/referral.js
@@ -11,14 +11,64 @@ var authentication = require('../lib/authentication');
 
 router.get('/',authentication.hasPermission(10),function(req,res,next){
 
-	res.send("yesh");
 
-});
-
-router.post('/create',authentication.hasPermission(10),function(req,res,next){
+	getAllReferrals(function(err,response){
+		if(err){
+			res.send("Error fetching referrals! " + err);
+		}
+		else
+		{
+			res.render('./referral/referral',{title: 'Referrals',referrals:response});
+		}
+	});
 
 	
+
 });
+
+var quick = function(finalResult){
+	return new Promise((resolve,reject) =>{
+		finalResult.ass = "shaved";
+		console.log("quick going");
+		resolve();
+	});
+};
+
+
+var getAllReferrals = function(callback)
+{
+	var finalResult = {};
+
+	// Promise.all([quick(finalResult)]).then(function(){
+	// 	console.log("Finished promises!");
+	// 	console.log(finalResult);
+	// })
+	// .catch(function(err){
+	// 	console.log("all promise error: " + err);
+	// });
+	database.db.query(
+		`SELECT
+			*,
+			(SELECT CONCAT(FName," ",LName) FROM ReferralPerson WHERE ReferralPerson.PersonID=Referral.Submitter) as SubmitterName,
+			Referral.DateAdded as ReferralDate,
+			(SELECT Email FROM ReferralPerson WHERE ReferralPerson.PersonID=Referral.Submitter) as SubmitterEmail,
+			(SELECT Phone FROM ReferralPerson WHERE ReferralPerson.PersonID=Referral.Submitter) as SubmitterPhone
+		FROM Referral INNER JOIN ReferralPerson ON Referral.Client=ReferralPerson.PersonID
+		ORDER BY ReferralID DESC`
+		,function(error,results,fields){
+		
+		if(error)
+		{
+			callback(error,null);
+		}
+		else
+		{
+			var returnObject = {};
+			returnObject.allReferrals = results;
+			callback(null,returnObject);
+		}
+	});
+}
 
 
 module.exports = router;
