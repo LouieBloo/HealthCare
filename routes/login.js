00fd2751689092e8ab342a5ac8c96e3f53367de6
LouieBloo <laleggio@ucdavis.edu>
2017-11-09 23:42:19 -0800

    Database work for new groups, new permission parsing done

--- routes/login.js
+++ routes/login.js
@@ -13,8 +13,9 @@ passport.use(new LocalStrategy(
 	function(username, password, done) {
 
 		database.db.query(`
-			SELECT User.UserID,Password,Salt,GROUP_CONCAT(UserPermissions.Permission SEPARATOR ',') as Permission FROM User
+			SELECT User.UserID,Password,Salt,GROUP_CONCAT(PermissionGroups.PermissionArray SEPARATOR ',') as Permission, User.PermissionExceptions FROM User
 			LEFT JOIN UserPermissions ON User.UserID=UserPermissions.UserID
+			LEFT JOIN PermissionGroups ON UserPermissions.Group=PermissionGroups.GroupID
 			WHERE Email=?
 			GROUP BY User.UserID
 			`,[username],function(error,results,fields){
@@ -40,17 +41,22 @@ passport.use(new LocalStrategy(
 						if(results[0].Password == hash)
 						{
 
-							var permissionSplit = [];
+							var permissionSplit = [];//results to be put into the passport user object
+							if(results[0].PermissionExceptions)//if the user has exceptions, add it to the permission groups
+							{
+								results[0].Permission += "," + results[0].PermissionExceptions;
+							}
 							if(results[0].Permission)
 							{
-								permissionSplit = results[0].Permission.split(",").map(Number);//takes the sql and returns a split string that is also cast as an int
+								var permissions = results[0].Permission.replace(/[^0-9,]/g,"");//remove everything but numbers and ,
+								permissionSplit = permissions.split(",").map(Number);//takes the results and turn them into ints
 							}
 
 							var user = {
 								id:results[0].UserID,
 								permission:permissionSplit
 							};
-							return done(null,user);
+							return done(null,user);//goes to passport and sets the cookie
 						}
 						else{
 							console.log("Error haasdfssword!");
